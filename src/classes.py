53d24b9fb61f8a08fcb8aaf54c723d9bbbcdb438
sergeyvakhrin <parlamentsv@mail.ru>
2024-05-25 23:47:31 +0300

    add func main() sort() and classes

--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Operation:
 
     def __init__(self, date: str, amount: str, currency_name: str, description: str, from_: str, to: str):
@@ -8,22 +10,42 @@ class Operation:
         self.from_ = from_
         self.to = to
 
-    def __srt__(self):
-        pass
+    def __str__(self):
+        return (f'{self.change_date_format()} {self.description}\n'
+                f'{self.hide_number_from()}{self.hide_number_to()}\n'
+                f'{self.amount} {self.currency_name}')
 
-    def __pepr__(self):
-        pass
+    # def __repr__(self):
+    #     pass
 
-    def sorted_instances(self):
+    def __lt__(self, other):
+        return self.date < other.date
+
+    def __gt__(self, other):
+        return self.date > other.date
+
+    def change_date_format(self):
         """
-
+        приобразовнаие даты в формат по заданию 14.10.2018
         """
-        pass
+        the_date = datetime.fromisoformat(self.date)
+        return the_date.strftime("%d.%m.%Y")
 
-    def get_quantity_operation(self, quantity):
+    def hide_number_from(self):
         """
-        Получение определенного размера списка экземпляров класса
+        скрытие номера счета списания
         """
-        self.quantity = quantity
+        if self.from_ != "":
+            hide_from = self.from_.split(" ")
+            hide_from[-1] = f'{hide_from[-1][:4]} {hide_from[-1][5:7]}** **** {hide_from[-1][-4:]} -> '
+            return " ".join(hide_from)
+        else:
+            return ""
 
-        return
+    def hide_number_to(self):
+        """
+        скрытие номера счета зачисления
+        """
+        hide_to = self.to.split(" ")
+        hide_to[-1] = f'**{hide_to[-1][-4:]}'
+        return " ".join(hide_to)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 from pathlib import Path
 
 from config import RESULT_OPERATOINS
@@ -12,30 +11,24 @@ def load_data(path: Path):
     """
 
     ###!!!! Добавить проверку наличия файла
-
     with open(path, "rt", encoding="UTF-8") as file:
         return json.load(file)
 
 
-def instances_executed(operations_list):
+def get_instances_executed(operations_list):
     """
     получаем список экземпляров классов успешных операций
     """
     instances_list: list = []
     for operation in operations_list:
         if operation.get("state") == RESULT_OPERATOINS:
-            # приобразовывание даты в формат по заданию 14.10.2018
-            date_: str = operation["date"]
-            the_date = datetime.fromisoformat(date_)
-            operation["date"] = the_date.strftime("%d.%m.%Y")
-            print(operation["date"])
             # инициализируем экземпляр класса
             operation = Operation(
                 date=operation["date"],
                 amount=operation["operationAmount"]["amount"],
                 currency_name=operation["operationAmount"]["currency"]["name"],
                 description=operation["description"],
-                from_=operation.get("from"),
+                from_=operation.get("from") if operation.get("from") is not None else "",
                 to=operation["to"]
             )
             # добавляем экземпляр класса в список
@@ -43,3 +36,10 @@ def instances_executed(operations_list):
         else:
             continue
     return instances_list
+
+def sorted_instances(instances_list_to_sort):
+    """
+    сортировка списка классов по дате
+    """
+    return sorted(instances_list_to_sort, reverse=True)
+
